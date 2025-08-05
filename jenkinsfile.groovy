6de9ac86b61d38e97c45e731a03b0174901b8426
vijai veerapandian <vijai.veerapandian@gmail.com>
2025-08-05 11:10:35 -0400

    work:updated

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -145,7 +145,7 @@ pipeline {
                 passwordVariable: 'DOCKER_PASSWORD'
             )]) {
                 sh '''
-                    echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME}--password-stdin
+                    echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
                     docker push ${env.DOCKER_IMAGE_NAME} --all-tags
                 '''
             }
